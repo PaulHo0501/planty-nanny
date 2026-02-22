e6bb9b1a0a184316ddf728adf989a9670ffffdab
Duc Ho <dtho@ualberta.ca>
2026-02-21 18:27:19 -0700

    Refactored comment code

--- esp32/Main/Main.ino
+++ esp32/Main/Main.ino
@@ -14,8 +14,6 @@ PlantyNanny pn;
 // Variables for non-blocking timer
 unsigned long lastSendTime = 0;
 const unsigned long sendInterval = 5000; // Send every 5 seconds
-
-// State flag to ensure we don't send data before STOMP is ready
 bool stompConnected = false;
 
 // --- STOMP FUNCTIONS ---
@@ -25,8 +23,6 @@ void sendStompConnect() {
     frame += "accept-version:1.2,1.1,1.0\n";
     frame += "heart-beat:10000,10000\n";
     frame += "\n"; 
-    
-    // Explicitly sending the null terminator is the secret sauce!
     webSocket.sendTXT((uint8_t *)frame.c_str(), frame.length() + 1);
 }
 
@@ -36,7 +32,6 @@ void sendStompSubscribe(String id, String destination) {
     frame += "destination:" + destination + "\n";
     frame += "ack:auto\n";
     frame += "\n";
-    
     webSocket.sendTXT((uint8_t *)frame.c_str(), frame.length() + 1);
 }
 
@@ -46,8 +41,7 @@ void sendStompSend(String destination, String jsonBody) {
     frame += "content-type:application/json\n";
     frame += "\n";
     frame += jsonBody;
-    frame += "\n"; // Good practice to end body with newline
-    
+    frame += "\n";
     webSocket.sendTXT((uint8_t *)frame.c_str(), frame.length() + 1);
 }
 
@@ -59,7 +53,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     switch(type) {
         case WStype_DISCONNECTED:
             Serial.println("[WS] Disconnected!");
-            stompConnected = false; // Reset flag
+            stompConnected = false;
             break;
 
         case WStype_CONNECTED:
@@ -71,24 +65,17 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             // 1. Handle STOMP CONNECTED
             if (text.startsWith("CONNECTED")) {
                 Serial.println("[STOMP] Connected successfully!");
-                stompConnected = true; // Set flag to true
-                
-                // Subscribe to your topic now
+                stompConnected = true;
                 sendStompSubscribe("0", "/topic/commands");
             }
-            
-            // 2. Handle STOMP MESSAGE (Incoming Data)
             else if (text.startsWith("MESSAGE")) {
-                // Robust way to find body: look for the double newline
                 int bodyIndex = text.indexOf("\n\n");
                 if (bodyIndex > 0) {
                     String body = text.substring(bodyIndex + 2);
-                    // Trim whitespace/nulls from the end just in case
                     body.trim(); 
                     Serial.print("[STOMP] Message Received: ");
                     Serial.println(body);
                     pn.processCommand(body);
-                    // TODO: Parse 'body' with ArduinoJson here
                 }
             }
             break;
@@ -117,23 +104,11 @@ void setup() {
     webSocket.begin(WS_HOST, WS_PORT, "/gs-guide-websocket"); 
     webSocket.onEvent(webSocketEvent);
     webSocket.setReconnectInterval(5000);
-    
+
+    pn.getCurrentLightStatus();
 }
 
 void loop() {
     // 1. Always keep the socket alive
     webSocket.loop();
-
-    // 2. Non-blocking timer to send data
-    // unsigned long now = millis();
-    // if (now - lastSendTime > sendInterval) {
-    //     lastSendTime = now;
-        
-    //     // ONLY send if we are actually connected to STOMP
-    //     if (stompConnected) {
-    //         Serial.println("Sending Heartbeat/Data...");
-    //         // Make sure to escape quotes inside the JSON string
-    //         sendStompSend("/app/command", "{\"type\":\"WATER\"}");
-    //     }
-    // }
 }
