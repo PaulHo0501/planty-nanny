5e716d0c1234ea78dfe6945551280612a358ad80
Thea Nguyen <thea25060@gmail.com>
2026-02-21 20:04:51 -0700

    Added humidity services

---
+++ server/src/main/java/com/doubletrouble/plantynanny/service/HumidityService.java
@@ -0,0 +1,29 @@
+package com.doubletrouble.plantynanny.service;
+
+
+import com.doubletrouble.plantynanny.repositorty.TreeRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HumidityService {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final TreeRepository treeRepository;
+
+    public HumidityService(SimpMessagingTemplate messagingTemplate, TreeRepository treeRepository) {
+        this.treeRepository = treeRepository;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void triggerEsp32Measurement() {
+        if (treeRepository.count() == 0) {
+            System.out.println("No plants registered. Skipping MEASUREMENT.");
+            return;
+        }
+        String commandJson = "{\"command\":\"MEASURE\", \"id\":\"esp32_cam_1\"}";
+        messagingTemplate.convertAndSend("/topic/commands", commandJson);
+        System.out.println("Sent MEASURE command to ESP32.");
+    }
+}
