922cf8c21deff93982a103ccc1fb4ff25082af71
Thea Nguyen <thea25060@gmail.com>
2026-02-21 18:06:27 -0700

    add light status server logic

---
+++ server/src/main/java/com/doubletrouble/plantynanny/repositorty/LightStatusRepository.java
@@ -0,0 +1,11 @@
+package com.doubletrouble.plantynanny.repositorty;
+
+import com.doubletrouble.plantynanny.entity.LightStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LightStatusRepository extends JpaRepository<LightStatus, UUID> {
+}
